d5cde114944dec9e082b133681b06f5a37dd78d9
Sachini Maheshika <maheshikasachini3@gmail.com>
2023-09-06 00:22:58 +0530

    update employee registration

--- src/main/webapp/js/company-register.js
+++ src/main/webapp/js/company-register.js
@@ -6,7 +6,9 @@ function clearInputs() {
 function saveEmp() {
     var empEmail = $('#email').val();
     var empPass = $('#pswd').val();
+    var empType;
 
+    //Empty check
     if (empPass == "" | empEmail == "") {
         $('#errorTitle').text("Employee New Login Account Error Message");
         $('#errorBody').text("Can't Create a New Login Account with Empty Inputs!!!");
@@ -14,6 +16,7 @@ function saveEmp() {
         clearInputs();
 
     } else {
+        //Login account check
         $.ajax({
             method:"POST",
             contentType:"application/json",
@@ -23,43 +26,82 @@ function saveEmp() {
                 if (data.code==="00" && data.message==="Employee-Found") {
                     $('#errorTitle').text("Employee New Login Account Error Message");
                     $('#errorBody').
-                    text("Employee already registered for Login Account. Please Login to your Account!!!");
+                    text("Employee already registered for Login Account. "+
+                    "Please Login to your Account!!!");
                     $('#modal-danger').modal('toggle');
                 } else {
-                    // Reg
-                    $.ajax({
-                        method:"POST",
-                        contentType:"application/json",
-                        url:"http://localhost:8080/oas/Emp/checkEmpByEmail/"+empEmail,
-                        async:true,
-                        success: function (data) {
-                            if (data.code==="00" && data.message==="Employee-Found") {
-                                $('#errorTitle').text("Employee New Login Account Error Message");
-                                $('#errorBody').
-                                text("Employee already registered for Login Account. Please Login to your Account!!!");
-                                $('#modal-danger').modal('toggle');
-                            } else {
-                                
-                            }
-                        },
-                        error: function (xhr, exception,response) {
-                            var error = eval("(" + xhr.responseText + ")");
-                            $('#errorTitle').text("Employee New Login Account Error Message");
-                            $('#errorBody').text("Account Creation Error!!");
-                            $('#modal-danger').modal('toggle');
-                
-                            clearInputs();
-                        }
-                    })
+                    
                 }
             },
             error: function (xhr, exception,response) {
-                var error = eval("(" + xhr.responseText + ")");
-                $('#errorTitle').text("Employee New Login Account Error Message");
-                $('#errorBody').text("Account Creation Error!!");
-                $('#modal-danger').modal('toggle');
-    
-                clearInputs();
+                // Employee email check in Employee datails
+                $.ajax({
+                    method:"POST",
+                    contentType:"application/json",
+                    url:"http://localhost:8080/oas/Emp/empByEmail/"+empEmail,
+                    //Employee Details email check
+                    async:true,
+                    success: function (data) {
+                        if (data.code==="00") {
+                            //Login account create
+                            //Employee type find
+                            $.ajax({
+                                method:"POST",
+                                contentType:"application/json",
+                                url:"http://localhost:8080/oas/Emp/empType/"+empEmail,
+                                async:true,
+                                success: function (data) {
+                                    empType = data.content;
+                                    $.ajax({
+                                        method:"POST",
+                                        contentType:"application/json",
+                                        url:"http://localhost:8080/oas/Emp/saveEmp",
+                                        async:true,
+                                        data:JSON.stringify({
+                                            "recId":"",
+                                            "empEmail":empEmail,
+                                            "empPassword":empPass,
+                                            "empType":empType
+                                        }),
+                                        success: function (data) {
+                                            $('#successTitle').text("Employee New Login Account Success Message");
+                                            $('#successBody').text("Login Account Save Successfully!!");
+                                            $('#modal-success').modal('toggle');
+                            
+                                            clearInputs();
+                                        },
+                                        error: function (xhr, exception,response) {
+                                            //console.log("Employee Save Error");
+                                            var error = eval("(" + xhr.responseText + ")");
+                                            $('#errorTitle').text("Employee New Login Account Error Message");
+                                            $('#errorBody').text("Login Account Save Error!!");
+                                            $('#modal-danger').modal('toggle');
+                            
+                                            clearInputs();
+                                        }
+                                    })
+                                },
+                                error: function (xhr, exception,response) {
+                                    $('#errorTitle').text("Employee New Login Account Error Message");
+                                    $('#errorBody').text("System Error. Try agian!!");
+                                    $('#modal-danger').modal('toggle');
+                        
+                                    clearInputs();
+                                }
+                            })
+                        } else {
+
+                        }
+                    },
+                    error: function (xhr, exception,response) {
+                        var error = eval("(" + xhr.responseText + ")");
+                        $('#errorTitle').text("Employee New Login Account Error Message");
+                        $('#errorBody').text("Employee doesn't exist!! 1234");
+                        $('#modal-danger').modal('toggle');
+            
+                        clearInputs();
+                    }
+                })
             }
         })
     }
